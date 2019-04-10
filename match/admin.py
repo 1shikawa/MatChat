8f94b1aa65afc822dfe3dde74602e74ec2fe22f2
1shikawa <ishikawa.toru@gmail.com>
2019-04-10 13:47:25 +0900

    2019/04/10 settings etc modify

--- match/admin.py
+++ match/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-# from models import userinfo
+from match.models.userinfo import UserInfo, Image
 
 
-# Register your models here.
-# class UserInfoAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'name',)  # 一覧に出したい項目
-#     list_display_links = ('name',)  # 修正リンクでクリックできる項目
-#
-#
-# admin.site.register(userinfo, UserInfoAdmin)
+class UserInfoAdmin(admin.ModelAdmin):
+    list_display = ('id','name',)  # 一覧に出したい項目
+    list_display_links = ('name',)  # 修正リンクでクリックできる項目
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('id','origin',)  # 一覧に出したい項目
+    list_display_links = ('id',)  # 修正リンクでクリックできる項目
+
+admin.site.register(UserInfo, UserInfoAdmin)
+admin.site.register(Image, ImageAdmin)
--- match/urls.py
+++ match/urls.py
@@ -6,14 +6,14 @@ app_name = 'match'
 
 urlpatterns = [
     path('', index.top, name='index'),
-    path('users/regist/', user.regist, name='regist'),
     path('login/', index.Login.as_view(), name='login'),
     path('logout/', index.Logout.as_view(), name='logout'),
     path('users/', user.gets, name='users'),
+    path('users/regist/', user.regist, name='regist'),
     path('users/profile/<int:user_id>', user.profile),
     path('users/<int:user_id>/edit/', user.edit),
     path('reactions/', reaction_controller.create, name='reactions'),
-    path('matching/', reaction.matching, name='matching'),
+    path('matching/', reaction.matching, name='matching'),  # 追加
     path('chat/create/<int:user_id>', chat.create, name='chat_create'),  # 追加
     path('chat/show/<int:room_id>', chat.show, name='chat_show'),  # 追加
     path('chat/show/<int:room_id>/messages/', chat.messages, name='chat_messages'),  # 追加
